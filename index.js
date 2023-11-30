05d213e767055934adf03c33fbc4768c3e9a3fbd
Viviane Dias <vivianedias@protonmail.com>
2023-11-30 11:27:10 -0300

    feat: checks when msg is submitted

--- index.js
+++ index.js
@@ -6,12 +6,40 @@ window.addEventListener("DOMContentLoaded", () => {
 	const emailInput = form.querySelector("input[type=email]");
 
 	emailInput.addEventListener("change", (e) => {
-		console.log("value", e.target.value);
 		const email = e.target.value;
 		console.log("email", email);
 		const password = process.env.SECRET_KEY;
-		encryptData(email, password).then((eData) =>
-			console.log("encryptedData", eData),
-		);
+		encryptData(email, password).then((eData) => {
+			window.BusaraEmailHash = eData;
+			console.log("encryptedData", eData);
+		});
 	});
+
+	const submitBtn = form.querySelector("button");
+
+	function handleFormSubmit() {
+		const hasInputs = form.getElementsByTagName("input").length > 0;
+
+		if (hasInputs) {
+			console.log("form ainda não submetido");
+			return false;
+		}
+
+		const allText = form.querySelectorAll("span");
+		const thankUMsgs = [...allText].filter((thankUMsg) =>
+			thankUMsg.textContent.includes("Recebemos seu pedido de ajuda"),
+		);
+
+		if (thankUMsgs.length > 0) {
+			console.log(thankUMsgs.map((msg) => console.log(msg.textContent)));
+			submitBtn.removeEventListener("click", handleFormSubmit);
+
+			return true;
+		}
+
+		console.log("não encontramos a msg de agradecimento");
+		return false;
+	}
+
+	submitBtn.addEventListener("click", handleFormSubmit);
 });
