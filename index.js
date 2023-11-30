52b8cec62343ab42b536779cbc1eae3445b6872f
Viviane Dias <vivianedias@protonmail.com>
2023-11-29 21:42:37 -0300

    feat: moves crypto import down

--- index.js
+++ index.js
@@ -4,29 +4,30 @@ const encryptionMethod = process.env.ENCRYPTION_METHOD
 const secretKey = process.env.SECRET_KEY
 const secretIv = process.env.SECRET_IV
 
-const key = crypto
-	.createHash('sha512')
-	.update(secretKey)
-	.digest('hex')
-	.substring(0, 32)
-const encryptionIV = crypto
-	.createHash('sha512')
-	.update(secretIv)
-	.digest('hex')
-	.substring(0, 16)
-
 window.addEventListener("DOMContentLoaded", function () {
 	console.log("content has loaded")
 	const form = document.getElementById("widget-16850")
 	const emailInput = form.querySelector("input[type=email]")
+
 	emailInput.addEventListener("change", (e) => {
 		console.log("value", e.target.value)
 		const email = e.target.value
+		console.log("email", email)
+
+		const key = crypto
+			.createHash('sha512')
+			.update(secretKey)
+			.digest('hex')
+			.substring(0, 32)
+		const encryptionIV = crypto
+			.createHash('sha512')
+			.update(secretIv)
+			.digest('hex')
+			.substring(0, 16)
 		const cipher = crypto.createCipheriv(encryptionMethod, key, encryptionIV)
 		const hash = Buffer.from(
 			cipher.update(email, 'utf8', 'hex') + cipher.final('hex')
 		).toString('base64')
-		console.log("email", email)
 		console.log("hash", hash)
 	})
 });
