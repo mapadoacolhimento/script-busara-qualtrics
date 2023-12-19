156ae31c54fe7bc65bd217447874d97c2403c1a0
Viviane Dias <vivianedias@protonmail.com>
2023-12-19 12:30:50 -0300

    chore: updates webpack to create two builds, one for server and one for client

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,8 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 const Dotenv = require("dotenv-webpack");
 const nodeExternals = require("webpack-node-externals");
 const ESLintPlugin = require("eslint-webpack-plugin");
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 
-const bundleOutputDir = "./dist";
 module.exports = (env) => {
 	const isDevBuild = !(env && env.prod);
 	const defaultPlugins = [
@@ -17,39 +17,69 @@ module.exports = (env) => {
 	];
 	const devPlugins = [...defaultPlugins, new ESLintPlugin()];
 
-	return [
-		{
-			entry: "./index.js",
-			output: {
-				filename: "widget.js",
-				path: path.resolve(bundleOutputDir),
-			},
-			devServer: {
-				static: bundleOutputDir,
-			},
-			plugins: isDevBuild ? devPlugins : defaultPlugins,
-			optimization: {
-				minimize: !isDevBuild,
-			},
-			mode: isDevBuild ? "development" : "production",
-			resolve: {
-				extensions: [".*", ".js"],
-			},
-			module: {
-				rules: [
-					{
-						test: /\.m?js$/,
-						exclude: /node_modules/,
-						use: {
-							loader: "babel-loader",
-							options: {
-								presets: ["@babel/preset-env"],
-							},
+	const defaultConfig = {
+		entry: "./index.js",
+		plugins: isDevBuild ? devPlugins : defaultPlugins,
+		optimization: {
+			minimize: !isDevBuild,
+		},
+		mode: isDevBuild ? "development" : "production",
+		module: {
+			rules: [
+				{
+					test: /\.m?js$/,
+					exclude: /node_modules/,
+					use: {
+						loader: "babel-loader",
+						options: {
+							presets: ["@babel/preset-env"],
 						},
 					},
-				],
-			},
-			externals: [nodeExternals({ importType: "var" })],
+				},
+			],
 		},
-	];
+	};
+
+	const serverConfig = {
+		...defaultConfig,
+		target: "node",
+		output: {
+			path: path.resolve(__dirname, "dist"),
+			filename: "widget.node.js",
+		},
+		resolve: {
+			extensions: [".*", ".js"],
+		},
+		externals: [nodeExternals()],
+	};
+
+	const clientConfig = {
+		...defaultConfig,
+		plugins: isDevBuild
+			? [
+					...devPlugins,
+					new NodePolyfillPlugin({
+						excludeAliases: ["crypto"],
+					}),
+			  ]
+			: [
+					...defaultPlugins,
+					new NodePolyfillPlugin({
+						excludeAliases: ["crypto"],
+					}),
+			  ],
+		target: "web",
+		output: {
+			path: path.resolve(__dirname, "dist"),
+			filename: "widget.js",
+		},
+		resolve: {
+			extensions: [".*", ".js"],
+			fallback: {
+				crypto: require.resolve("crypto-browserify"),
+			},
+		},
+	};
+
+	return [serverConfig, clientConfig];
 };
