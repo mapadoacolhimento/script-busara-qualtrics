da47350f2f9b4a77b7cfaede2b905155d92c8a58
Viviane Dias <vivi@mapa.org.br>
2024-05-27 20:03:26 -0300

    feat(decrypt): adds try/catch to decrypt

--- src/decrypt.js
+++ src/decrypt.js
@@ -1,13 +1,18 @@
 import crypto from "crypto";
 
 export default function decryptData(encryptedData, key, iv) {
-	const encryptionMethod = process.env.ENCRYPTION_METHOD;
+	try {
+		const encryptionMethod = process.env.ENCRYPTION_METHOD;
 
-	const buff = Buffer.from(encryptedData, "base64");
-	// Converts encrypted data to utf8
-	const decipher = crypto.createDecipheriv(encryptionMethod, key, iv);
-	return (
-		decipher.update(buff.toString("utf8"), "hex", "utf8") +
-		decipher.final("utf8")
-	); // Decrypts data and converts to utf8
+		const buff = Buffer.from(encryptedData, "base64");
+		// Converts encrypted data to utf8
+		const decipher = crypto.createDecipheriv(encryptionMethod, key, iv);
+		return (
+			decipher.update(buff.toString("utf8"), "hex", "utf8") +
+			decipher.final("utf8")
+		); // Decrypts data and converts to utf8
+	} catch (e) {
+		console.error("error:", e);
+		return null;
+	}
 }
